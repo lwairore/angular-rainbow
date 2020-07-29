4d20bcc60bf4b10a96a62f25159e4d50421ee70a
lwairore <kwairore@gmail.com>
2020-07-29 15:24:08 +0300

    - Use HostBinding to set properties on the element and HostListener to listen for keydown event.

--- src/app/rainbow.directive.ts
+++ src/app/rainbow.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener
+} from '@angular/core';
 
 @Directive({
   selector: '[appRainbow]'
 })
 export class RainbowDirective {
+  possibleColors = [
+    'darksalmon', 'hotpink', 'lightskyblue', 'goldenrod', 'peachpuff',
+    'mediumspringgreen', 'cornflowerblue', 'blanchedalmond', 'lightslategrey'
+  ];
+  @HostBinding('style.color') color: string;
+  @HostBinding('style.border-color') borderColor: string;
+  @HostListener('keydown') newColor() {
+    const colorPick = Math.floor(Math.random() * this.possibleColors.length);
+    this.color =  this.possibleColors[colorPick];
+    this.borderColor = this.possibleColors[colorPick];
+  }
 
-  constructor() { }
+  constructor(){}
 
 }
