23addff64797293876a4610cca70b59056b9c9af
lwairore <kwairore@gmail.com>
2020-08-11 11:15:36 +0300

    - Wrap input element in main element.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <input type="text" appRainbow>
+  <main class="container">
+    <input 
+          class="form-control form-control-lg d-flex justify-content-center" 
+          type="text" 
+          placeholder="Start typing..." 
+          aria-label="Start typing... example"
+          [ngStyle]="{
+            'width': '100%',
+            'max-width': '330px',
+            'padding': '15px',
+            'margin': 'auto'
+          }"
+          appRainbow>
+  </main>
   `,
   styleUrls: ['./app.component.css']
 })
